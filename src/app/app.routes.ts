19dc59a200c7f4fe83b950b2c8334fdaeb4ff1ea
CasasolaCano <casasolacontact@gmail.com>
2021-03-22 12:36:57 +0100

    Separate child user routes on user folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,20 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
-import { NewUserComponent } from './components/user/new-user.component';
-import { EditUserComponent } from './components/user/edit-user.component';
-import { DetailUserComponent } from './components/user/detail-user.component';
+import { USER_ROUTES } from './components/user/user.routes';
 
 
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'user/:id', component: UserComponent, children: [
-        {path: 'new', component: NewUserComponent},
-        {path: 'edit', component: EditUserComponent},
-        {path: 'detail', component: DetailUserComponent},
-        {path: '', component: DetailUserComponent}
-    ]},
+    { path: 'user/:id', component: UserComponent, children: USER_ROUTES},
     { path: '', component: HomeComponent}
 ];
 
