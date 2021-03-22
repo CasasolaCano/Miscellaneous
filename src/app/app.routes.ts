725b994d88ece789aa55455408cdabb82b639356
CasasolaCano <casasolacontact@gmail.com>
2021-03-22 12:32:24 +0100

    Child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,20 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
+import { NewUserComponent } from './components/user/new-user.component';
+import { EditUserComponent } from './components/user/edit-user.component';
+import { DetailUserComponent } from './components/user/detail-user.component';
+
 
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'user', component: UserComponent },
+    { path: 'user/:id', component: UserComponent, children: [
+        {path: 'new', component: NewUserComponent},
+        {path: 'edit', component: EditUserComponent},
+        {path: 'detail', component: DetailUserComponent},
+        {path: '', component: DetailUserComponent}
+    ]},
     { path: '', component: HomeComponent}
 ];
 
